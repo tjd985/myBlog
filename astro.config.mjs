c93814cbb8bc1800e718cc7be73addc3948be00c
tjd985 <tjd985@gmail.com>
2025-06-19 00:50:00 +0900

    [Core] intergrate tailwind and preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://seonghodev.netlify.app/',
   integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
