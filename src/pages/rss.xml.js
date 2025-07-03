54f509ae9212848dca3e9368b573898bcfe2302c
tjd985 <tjd985@gmail.com>
2025-07-04 00:45:45 +0900

    style: ul스타일 추가

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   return rss({
     title: 'seonghoDev',
     description: "OH SEONGHO's dev bolg",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await getCollection('post'),
     customData: `<language>kr</language>`,
   });
 }
